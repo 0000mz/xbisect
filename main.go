33fc4fc25bbe09358cbae63140504568e5b94808
muiz <muiz.dev@gmail.com>
2024-12-31 07:06:12 -0800

    Log the hash of the current bisect step
    Also ensure proper cleanup by returning instead of immediate exit on
    failures.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -72,6 +74,11 @@ func SetupLoggerOrDie(verbose bool) {
 		Foreground(lipgloss.Color("0"))
 	styles.Keys["err"] = lipgloss.NewStyle().Foreground(lipgloss.Color("204"))
 	styles.Values["err"] = lipgloss.NewStyle().Bold(true)
+	styles.Levels[charmlog.InfoLevel] = lipgloss.NewStyle().
+		SetString(">").
+		Padding(0, 1, 0, 1).
+		Foreground(lipgloss.Color("#38f2ae")).
+		Bold(true)
 
 	gConsoleLogger = charmlog.NewWithOptions(os.Stdout, charmlog.Options{
 		ReportCaller:    false,
@@ -197,26 +204,26 @@ func filepathExists(filepath string) bool {
 	return err == nil // !os.IsNotExist(err)
 }
 
-func ImportGitRepo(repo_url string, name string) {
+func ImportGitRepo(repo_url string, name string) bool {
 	if len(name) == 0 {
 		ConsoleLogError("--name not specified for repo import.")
-		os.Exit(1)
+		return false
 	}
 	if matched, err := regexp.MatchString("^[a-zA-Z0-9_-]+$", name); !matched || err != nil {
 		ConsoleLogError("Invalid repo name. Only alphanumeric and underscore/dash allowed.")
 		if err != nil {
 			gLogger.Printf("Regex error: %v\n", err)
 		}
-		os.Exit(1)
+		return false
 	}
 	name = strings.ToLower(name)
 	if len(repo_url) == 0 {
 		ConsoleLogError("--git is empty.")
-		os.Exit(1)
+		return false
 	}
 	if gConfig.HasRepo(name) {
 		ConsoleLogError("Repo \"%s\" already exists.", name)
-		os.Exit(1)
+		return false
 	}
 
 	var err error
@@ -226,7 +233,7 @@ func ImportGitRepo(repo_url string, name string) {
 	if err = os.RemoveAll(clonedir); err != nil {
 		gLogger.Printf("Error: %v", err)
 		ConsoleLogError("System error")
-		os.Exit(1)
+		return false
 	}
 
 	ConsoleLogInfo("Cloning git repo: %s", repo_url)
@@ -234,24 +241,30 @@ func ImportGitRepo(repo_url string, name string) {
 	if err != nil {
 		gLogger.Printf("Error: %v\n", err)
 		ConsoleLogError("Git clone failed")
-		os.Exit(1)
+		return false
 	}
 	gConfig.AddRepo(name, clonedir, repo_url)
+	return true
 }
 
-func CleanCache() {
+func CleanCache() bool {
 	cachedir := path.Join(GetAppDataDir(), "cache")
 	err := os.RemoveAll(cachedir)
 	if err != nil {
 		gLogger.Printf("Error: %v\n", err)
 		ConsoleLogError("Error occurred when removing cache dir")
-		os.Exit(1)
+		return false
 	} else {
 		ConsoleLogInfo("Successfully cleaned up cache.")
 	}
+	return true
 }
 
 func runCommand(command ...string) error {
+	return runCommandDir("", command...)
+}
+
+func runCommandDir(dir string, command ...string) error {
 	if len(command) < 1 {
 		return fmt.Errorf("Empty command")
 	}
@@ -259,15 +272,19 @@ func runCommand(command ...string) error {
 	cmd := exec.Command(command[0], command[1:]...)
 	cmd.Stdout = gLogFileHandler
 	cmd.Stderr = gLogFileHandler
+	if len(dir) > 0 {
+		cmd.Dir = dir
+	}
 	return cmd.Run()
+
 }
 
-func RunBisect(reponame string) {
+func RunBisect(reponame, lo, hi string) bool {
 	repo := gConfig.GetRepo(reponame)
 	if repo == nil {
 		ConsoleLogError("No imported repo with name: \"%s\". Run %s import --help",
 			reponame, kApplicationName)
-		os.Exit(1)
+		return false
 	}
 
 	cachedir := ""
@@ -285,19 +302,126 @@ func RunBisect(reponame string) {
 	if err != nil {
 		gLogger.Printf("Error: %v\n", err)
 		ConsoleLogError("Failed to create cache dir: %s", cachedir)
-		os.Exit(1)
+		return false
 	}
-	ConsoleLogInfo("Using cache directory for bisect: %s\n", cachedir)
+	ConsoleLogInfo("Using cache directory for bisect: %s", cachedir)
 
 	// Copy the repo source to the cache location.
+	cacherepo := path.Join(cachedir, "_repo")
 	{
-		cacherepo := path.Join(cachedir, "_repo")
 		if err = runCommand("cp", "--recursive", repo.LocalPath, cacherepo); err != nil {
 			gLogger.Printf("Error: %v\n", err)
 			ConsoleLogError("Failed to copy repo to cache location.")
-			os.Exit(1)
+			return false
 		}
 	}
+
+	ConsoleLogInfo("Lo: %s", lo)
+	ConsoleLogInfo("Hi: %s", hi)
+
+	// DBG: Create tempfile for the script that will be executed in the bisect
+	// operation.
+	tmpfile, err := os.CreateTemp("", "bisect_script")
+	if err != nil {
+		ConsoleLogError("Failed to create temp bisect script")
+		return false
+	}
+
+	{
+		script := `
+		echo "Running bisect on current hash"
+		echo "cwd: $(pwd)"
+		go run . > /tmp/compute 2>&1
+		cat /tmp/compute
+		`
+		if _, err = tmpfile.WriteString(script); err != nil {
+			ConsoleLogError("Failed to write script data to tempfile")
+			gLogger.Printf("Error: %v\n", err)
+			return false
+		}
+		tmpfile.Close()
+		runCommand("chmod", "+x", tmpfile.Name()) // Give exec perms
+	}
+
+	command_sequence := [][]string{
+		// Ensure that no bisect is running. This will do nothing if
+		// it is not in bisect mode.
+		{"git", "bisect", "reset"},
+		{"git", "bisect", "start"},
+		// TODO: The good and bad are not always synonymous w/ lo and hi commit hash...
+		{"git", "bisect", "good", lo},
+		{"git", "bisect", "bad", hi},
+	}
+
+	for _, cmd := range command_sequence {
+		if err = runCommandDir(cacherepo, cmd...); err != nil {
+			gLogger.Printf("Error: %v\n", err)
+			ConsoleLogError("Error setting up bisect state.")
+			return false
+		}
+	}
+	ConsoleLogInfo("Running bisect script")
+	defer func() {
+		gLogger.Println("Resetting git bisect")
+		runCommandDir(cacherepo, "git", "bisect", "reset")
+	}()
+	{
+		cmd := exec.Command("git", "bisect", "run", tmpfile.Name())
+		cmd.Dir = cacherepo
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			gLogger.Printf("Error: %v\n", err)
+			ConsoleLogError("Error occurred setting up git bisect output streaming.")
+			return false
+		}
+		if err = cmd.Start(); err != nil {
+			gLogger.Printf("Error: %v\n", err)
+			ConsoleLogError("Failed to start git bisect")
+			return false
+		}
+
+		// Use a teewriter to output to the logfile and also scan the
+		// output.
+		var buf bytes.Buffer
+		tee := io.TeeReader(stdout, &buf)
+
+		hashLineRe := regexp.MustCompile(`^\[(.*)\] .*$`)
+
+		scanner := bufio.NewScanner(tee)
+		var lines_until_hash int64 = 0
+
+		var scan_succeed bool
+		scan_succeed = true
+
+		for scanner.Scan() {
+			lines_until_hash -= 1
+
+			line := scanner.Text()
+			if matches, _ := regexp.MatchString("^Bisecting: [0-9]+ revision(s)? left to test after this \\(roughly [0-9]+ step(s)?\\)$", line); matches {
+				lines_until_hash = 1
+			} else if lines_until_hash == 0 {
+				hashes := hashLineRe.FindStringSubmatch(line)
+				if len(hashes) != 2 {
+					ConsoleLogError("Failed to parse log of git message")
+					scan_succeed = false
+					break
+				}
+				hash := hashes[1]
+				ConsoleLogInfo("Hash %s", hash)
+			}
+		}
+		buf.WriteTo(gLogger.Writer())
+		if !scan_succeed {
+			return false
+		}
+
+		if err = cmd.Wait(); err != nil {
+			gLogger.Printf("Error: %v\n", err)
+			ConsoleLogError("Failed to run git bisect")
+			return false
+		}
+	}
+	return true
 }
 
 var cli struct {
@@ -305,6 +429,8 @@ var cli struct {
 
 	Run struct {
 		Repo string `help:"Run bisect operation for the given project." short:"r"`
+		Lo   string `help:"Hash of the earlier commit."`
+		Hi   string `help:"Hash of the later commit."`
 	} `cmd:"" help:"Run a bisect operation"`
 
 	Import struct {
@@ -316,7 +442,7 @@ var cli struct {
 	} `cmd:"" help:"Clean up the cache."`
 }
 
-func main() {
+func Main() int {
 	ctx := kong.Parse(&cli,
 		kong.Name(kApplicationName),
 		kong.Description(kApplicationDescrption),
@@ -336,12 +462,21 @@ func main() {
 		gConfig.Save()
 	}()
 
+	var success bool = false
 	switch ctx.Command() {
 	case "import":
-		ImportGitRepo(cli.Import.Git, cli.Import.Name)
+		success = ImportGitRepo(cli.Import.Git, cli.Import.Name)
 	case "run":
-		RunBisect(cli.Run.Repo)
+		success = RunBisect(cli.Run.Repo, cli.Run.Lo, cli.Run.Hi)
 	case "clean":
-		CleanCache()
+		success = CleanCache()
 	}
+	if !success {
+		return 1
+	}
+	return 0
+}
+
+func main() {
+	os.Exit(Main())
 }
